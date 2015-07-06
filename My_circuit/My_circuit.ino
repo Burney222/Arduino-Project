286d6654e949e93d11e1926aa2d48088baeff220
Daniel Berninghoff <daniel.berninghoff@tu-dortmund.de>
2015-07-06 13:54:48 +0200

    Added third (colored) LED

--- My_circuit/My_circuit.ino
+++ My_circuit/My_circuit.ino
@@ -6,9 +6,15 @@ const int button2Pin = 3;  // pushbutton 2 pin
 const int led1Pin = 12;    // LED 1 pin
 const int led2Pin = 13;    // LED 2 pin
 
-const int buzzerPin = 9;   // buzzer pin
+const int led3Pin_blue = 9;  // LED 3 pins
+const int led3Pin_green = 10;
+const int led3Pin_red  = 6;
+
+const int buzzerPin = 5;   // buzzer pin
 
 int nButtons = 2;
+
+int highscore1;  //variable to hold the highscore of game 1
 
 
 void setup()
@@ -20,6 +26,9 @@ void setup()
   // Set up the LED pins to be an output:
   pinMode(led1Pin, OUTPUT);
   pinMode(led2Pin, OUTPUT);
+  pinMode(led3Pin_red, OUTPUT);
+  pinMode(led3Pin_green, OUTPUT);
+  pinMode(led3Pin_blue, OUTPUT);
   
   // Buzzer output
   pinMode(buzzerPin, OUTPUT);
@@ -29,6 +38,9 @@ void setup()
   
   // Serial Output
   Serial.begin(9600);
+  
+  // Set highscore1 to zero
+  highscore1 = 0;
 }
 
 
@@ -114,7 +126,7 @@ int resetstatus(int button1State, int button2State)
   }
   else
   {
-    return 0;              //or do nothing an return 0
+    return 0;              //or do nothing and return 0
   }
 }
 
@@ -122,13 +134,12 @@ int resetstatus(int button1State, int button2State)
 //Blinking and/or tone patterns
 void blinkrightinput() {
   for(int i = 0; i < 2; i++) {    
-    digitalWrite(led2Pin, HIGH);
+    digitalWrite(led3Pin_green, HIGH);
     delay(100);
-    digitalWrite(led2Pin, LOW);
+    digitalWrite(led3Pin_green, LOW);
     delay(100);        //if everything was correct: blink yellow led and the game is on
   }
 }
-
 
 void blink_tone_wronginput() {
   digitalWrite(led1Pin, HIGH);
@@ -142,21 +153,68 @@ void blink_tone_wronginput() {
   digitalWrite(led1Pin, LOW);
 }
 
-
 void blinknewgame() {
   for(int i = 0; i < 3; i++) {
     digitalWrite(led1Pin, HIGH);
     digitalWrite(led2Pin, HIGH);
+    digitalWrite(led3Pin_green, HIGH);
     delay(100);
     digitalWrite(led1Pin, LOW);
     digitalWrite(led2Pin, LOW);
+    digitalWrite(led3Pin_green, LOW);
     delay(100);
   }
   delay(700);
 }
 
 
+void blink_tone_newhighscore() {
+  char notes[] = "cdef";
+  int beats[] = {2,2,2,2};
+  int songLength = 4;
+  int tempo = 100;
+  
+  for (int i = 0; i < songLength; i++) // step through the song arrays
+  {
+    int duration = beats[i] * tempo;  // length of note/rest in ms
+    if (i==3) duration *= 3;
+    tone(buzzerPin, frequency(notes[i]), duration);
+    showRainbow(i*60);
+    delay(duration);              // wait for tone to finish
+    delay(tempo/10);              // brief pause between notes
+  }
+  digitalWrite(led3Pin_red, LOW);
+  digitalWrite(led3Pin_green, LOW);
+  digitalWrite(led3Pin_blue, LOW);
+  delay(1500);
+}
+  
+  
+// Play melody
+// takes string of notes and spaces (=rests) and array of beats (A "1" represents a quarter-note, 2 a half-note, etc.,)
+// Note that you also need to provide the songLength (= length of notes and spaces = length of array of beats)
+// Also note that spaces also need a beat!
+// tempo is optional and describes the overall tempo of the melody
+void Melody(char notes[], int beats[], int songLength, int tempo) {
+  for (int i = 0; i < songLength; i++) // step through the song arrays
+  {
+    int duration = beats[i] * tempo;  // length of note/rest in ms
+    
+    if (notes[i] == ' ')          // is this a rest? 
+    {
+      delay(duration);            // then pause for a moment
+    }
+    else                          // otherwise, play the note
+    {
+      tone(buzzerPin, frequency(notes[i]), duration);
+      delay(duration);            // wait for tone to finish
+    }
+    delay(tempo/10);              // brief pause between notes
+  }
+}
 
+
+// Frequency function
 int frequency(char note) 
 {
   // This function takes a note character (a-g), and returns the
@@ -180,25 +238,72 @@ int frequency(char note)
 }
 
 
-
-
-
-
-
-//Loop = main-function
-void loop()
+void showSpectrum(int wait /*delaytime in ms*/)
 {
-  int button1State, button2State;  // variables to hold the pushbutton states
-  int reset;  // variable to hold whether the reset condition was fulfilled or not
+  int x;  // define an integer variable called "x"
 
-  //Read status of the LEDs
-  button1State = digitalRead(button1Pin);
-  button2State = digitalRead(button2Pin);
-
-
-  //Start game1
-  boolean game1 = true;
+  for (x = 0; x < 255; x++) {  //up
+    showRainbow(x);  // Call showRainbow() with our new x
+    delay(wait);   // Delay for 10 ms (1/100th of a second)
+  }
   
+  digitalWrite(led3Pin_red, LOW);
+  digitalWrite(led3Pin_green, LOW);
+  digitalWrite(led3Pin_blue, LOW);
+}
+
+
+
+void showRainbow(int color) {  //takes color-value from 0 to 254
+  int Phase = color / 51;
+  int Level = color % 51;
+  int Red, Green, Blue;
+  switch( Phase ) {
+     case 0:
+       Red   = 255;
+       Green = Level * 5;
+       Blue  = 0;
+       break;
+
+     case 1:
+       Red   = 255 - Level * 5;
+       Green = 255;
+       Blue  = 0;
+       break;
+
+     case 2:
+       Red   = 0;
+       Green = 255;
+       Blue  = Level * 5;
+       break;
+
+     case 3:
+       Red   = 0;
+       Green = 255 - Level * 5;
+       Blue  = 255;
+       break;
+
+     case 4:
+       Red   = Level * 5;
+       Green = 0;
+       Blue  = 255;
+       break;
+
+     default:
+       Red = Green = Blue = 255;
+   }
+  analogWrite(led3Pin_red, Red);
+  analogWrite(led3Pin_blue, Blue);
+  analogWrite(led3Pin_green, Green);
+}
+
+
+//game1 function
+//starts the single-player game and returns the score
+int game1(int button1State, int button2State) {
+  boolean game1 = true;
+  int score; 
+
   long randomnumber;
   Vector<long> random_numbers;
   blinknewgame();
@@ -232,7 +337,7 @@ void loop()
     
     while(get_input) {
       long buttonnumber = 0;
-      //Read status of the LEDs
+      //Read status of the buttons
       button1State = digitalRead(button1Pin);
       button2State = digitalRead(button2Pin);
       lightLEDs(button1State, button2State);
@@ -274,13 +379,39 @@ void loop()
     if (wrong_input) {
       blink_tone_wronginput();
       game1 = false;  
-      Serial.println("GAME OVER"); //if something wrong was entered in the get_input-loop: light red led and the game is over
     }
     if (!wrong_input) {
       blinkrightinput();
     }
     delay(1500);
-  } 
     
+    //leave this while-loop if game1 = false
+  }
+  Serial.println("GAME OVER"); //if something wrong was entered in the get_input-loop: light red led and the game is over
+  Serial.println("SCORE:");
+  Serial.println(score);
+  score = random_numbers.size()-1;
+  return score;
+} 
+
+
+
+
+
+//Loop = main-function
+void loop()
+{
+  int button1State, button2State;  // variables to hold the pushbutton states
+  int score1;
+  
+  //Start game1
+  score1 = game1(button1State, button2State); 
+  if (score1 > highscore1) {
+    highscore1 = score1;
+    Serial.println("NEW HIGHSCORE!");
+    blink_tone_newhighscore();
+    
+  }
+   
 
 }
